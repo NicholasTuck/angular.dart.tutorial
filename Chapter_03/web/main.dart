b9924ee5e79a426ecb4b78e23b9d123552d44fe2
Nicholas Tuck <nick@nicholastuck.com>
2014-07-28 16:43:28 -0500

    Update chapter 03 to use animation to show angular animation bug

--- Chapter_03/web/main.dart
+++ Chapter_03/web/main.dart
@@ -1,6 +1,7 @@
 library recipe_book;
 
 import 'package:angular/angular.dart';
+import 'package:angular/animate/module.dart';
 import 'package:angular/application_factory.dart';
 
 import 'package:angular_dart_demo/rating/rating_component.dart';
@@ -8,8 +9,9 @@ import 'package:angular_dart_demo/recipe_book.dart';
 
 class MyAppModule extends Module {
   MyAppModule() {
-    type(RecipeBookController);
-    type(RatingComponent);
+    install(new AnimationModule());
+    bind(RecipeBookController);
+    bind(RatingComponent);
   }
 }
 
